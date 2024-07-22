463ce144bf5c05659c4b244d7f2283ed0a58be7f
adrian lara <adrianlara203@gmail.com>
2024-07-22 11:53:02 -0500

    add orden and prodcuto endpoints

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import bodyParser from "body-parser";
 
 import clienteRoute from "./routes/clienteRoute.js";
 import categoriaRoute from "./routes/categoriaRoute.js";
+import productoRoute from "./routes/productoRoute.js";
+import ordenRoute from "./routes/ordenRoute.js";
 
 export const app = express();
 
@@ -14,6 +16,10 @@ app.use(bodyParser.json());
 app.use("/api/v0/cliente", clienteRoute);
 
 app.use("/api/v0/categoria", categoriaRoute);
+
+app.use("/api/v0/producto", productoRoute);
+
+app.use("/api/v0/orden", ordenRoute);
 
 app.get("/", (req, res) => {
   return res.status(201).json({ msg: "alive" });
